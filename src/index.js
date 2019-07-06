bf88af6404b26ca24adb32049becdec36ee2b9c9
Shimonure <rogelio.vargas.marquez@gmail.com>
2019-07-06 10:51:22 -0500

    Hello World with Graphql

--- src/index.js
+++ src/index.js
@@ -1,10 +1,27 @@
 import express from 'express';
+import graphqlHTTP from 'express-graphql';
+
+import schema from './schema';
 
 const app = express();
 const port = 8080;
 
 app.get('/', (request, response) => {
-    response.send('Hello Word, GraphQL practices');
+    response.send('Hello World Web');
 });
+
+// This is the resolver
+const root = {hola: () => "Hello World GraphQL"}
+
+app.use('/graphql', graphqlHTTP({
+    //Schema: indicate what schema will use
+    schema: schema,
+    //Resolver: The resolver is indicate in rootValue
+    rootValue: root,
+    //We use graphiql
+    graphiql: true
+
+}));
+
 
 app.listen(port, () => console.log(`El server esta funcionando en el puerto: ${port}`));
